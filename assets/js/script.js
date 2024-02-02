c3f701aa8c49520c7d0bdbaafc340c5eaa3b80f5
Juan Pablo M. <pablochileidh@gmail.com>
2024-02-02 18:00:16 -0300

    actualizacion y resolucion de problema bucle ventas

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,12 +91,53 @@ const propiedades_venta = [
     pets: true
     }
 ]
-
 let elementoVenta = document.getElementById("venta");
 let elementoAlquiler = document.getElementById("alquiler");
 
 function actualizarVenta() {
-    elementoVenta.innerHTML = "<p>Hola <strong>desde Venta</strong>!</p>";
+    let contenidoTotal = ""
+    let contenidoHtml =""
+    for(let i = 0; i< propiedades_alquiler.length; i++){
+        // Se ejecuta 5 veces, con valores del paso 0 al 4.
+        console.log(propiedades_venta[i].nombre);
+    contenidoHtml +=`
+    <div class="col-md-4 mb-4">
+    <div class="card">
+      <img
+        src="https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg"
+        class="card-img-top"
+        alt="Imagen del departamento"
+      />
+      <div class="card-body">
+        <h5 class="card-title">
+          ${propiedades_venta[i].nombre}
+        </h5>
+        <p class="card-text">
+          Este apartamento de lujo está ubicado en una exclusiva zona
+          residencial
+        </p>
+        <p>
+          <i class="fas fa-map-marker-alt"></i> 123 Luxury Lane,
+          Prestige Suburb, CA 45678
+        </p>
+        <p>
+          <i class="fas fa-bed"></i> 4 Habitaciones |
+          <i class="fas fa-bath"></i> 4 Baños
+        </p>
+        <p><i class="fas fa-dollar-sign"></i> 5.000</p>
+        <p class="text-danger">
+          <i class="fas fa-smoking-ban"></i> No se permite fumar
+        </p>
+        <p class="text-danger">
+          <i class="fa-solid fa-ban"></i> No se permiten mascotas
+        </p>
+      </div>
+    </div>
+  </div>
+    `
+ }
+      contenidoTotal = "<div class=row>" + contenidoHtml + "</div>"
+      elementoVenta.innerHTML = contenidoTotal;
 }
 
 function actualizarAlquiler() {
